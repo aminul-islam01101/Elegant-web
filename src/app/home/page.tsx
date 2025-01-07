806120606ae0fd7cb310df85133051080309507d
Forhad Khan <fk.nafi11@gmail.com>
2025-01-07 17:09:57 +0600

    markdeting brands done

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,7 @@
 import AboutUs from "@/components/view/AboutUs/AboutUs";
 import Banner from "@/components/view/Banner/Banner";
 import DataServices from "@/components/view/DataServices/DataServices";
+import MarketingBrands from "@/components/view/MarketingBrands/MarketingBrands";
 import Marquee from "@/components/view/Marquee/Marquee";
 import React from "react";
 
@@ -11,6 +12,7 @@ export default function HomePage() {
       <AboutUs />
       <DataServices />
       <Marquee />
+      <MarketingBrands />
     </div>
   );
 }
