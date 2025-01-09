fe1be415ae68284c5a4ea3f01feca439baae74e7
Forhad Khan <fk.nafi11@gmail.com>
2025-01-09 15:56:44 +0600

    footer done

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import logo from "../../../../public/assets/marketing7.png";
+import globe from "../../../../public/assets/globe1.png";
 import Link from "next/link";
 
 export default function Footer() {
@@ -21,7 +22,7 @@ export default function Footer() {
         {/* horizontal line */}
         <hr className="px-1 my-10" />
         {/* logo and links */}
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-10 justify-between items-center mx-auto">
           {/* logo and paragraph */}
           <div className="flex flex-col space-y-4">
             <Image src={logo} width={141} height={75} alt="LOGO" />
@@ -31,45 +32,69 @@ export default function Footer() {
             </p>
           </div>
           {/* navigate links */}
-          <div className="flex border w-full justify-between">
+          <div className="flex w-full justify-between">
             {/* pages */}
-            <div className="flex flex-col">
+            <div className="flex flex-col space-y-2">
               <span className="text-lg text-[#DD4242] font-semibold">
                 Pages
               </span>
-              <Link href="" className="link link-hover">
+              <Link href="/" className="text-sm">
                 Branding
               </Link>
-              <Link href="/" className="link link-hover">
+              <Link href="/" className="text-sm">
                 Design
               </Link>
-              <Link href="/" className="link link-hover">
+              <Link href="/" className="text-sm">
                 Marketing
               </Link>
-              <Link href="/" className="link link-hover">
+              <Link href="/" className="text-sm">
                 Advertisement
               </Link>
             </div>
             {/* Services */}
-            <div className="flex flex-col">
+            <div className="flex flex-col space-y-2">
               <span className="text-lg text-[#DD4242] font-semibold">
                 Services
               </span>
-              <Link href="" className="link link-hover">
+              <Link href="/" className="text-sm">
                 Branding
               </Link>
-              <Link href="/" className="link link-hover">
+              <Link href="/" className="text-sm">
                 Design
               </Link>
-              <Link href="/" className="link link-hover">
+              <Link href="/" className="text-sm">
                 Marketing
               </Link>
-              <Link href="/" className="link link-hover">
+              <Link href="/" className="text-sm">
                 Advertisement
               </Link>
             </div>
           </div>
           {/* image */}
+          <div className="flex justify-center items-center">
+            <Image src={globe} width={418} height={415} alt="globe" />
+          </div>
+          {/* address line */}
+          <div className="flex flex-col space-y-4">
+            <span className="text-lg text-[#DD4242] font-semibold">
+              Elegant Web Studio
+            </span>
+            <p className="primary-text">
+              3F Mitaka Takagi Building, <br /> Musashino-shi,80-0006 Japan{" "}
+              <br />
+              Info@exampel.com
+            </p>
+          </div>
+        </div>
+        {/* horizontal line */}
+        <hr className="px-1 my-10" />
+        <div className="flex justify-between mb-5">
+          <span className="primary-text">
+            All Right Reserved © 2025 ELagent Web Studio
+          </span>
+          <span className="primary-text">
+            Privacy Policy / Terms & Conditions
+          </span>
         </div>
       </div>
     </footer>
