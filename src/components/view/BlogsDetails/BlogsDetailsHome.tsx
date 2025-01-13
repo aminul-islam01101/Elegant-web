9e6479695cfd86894de3e0fa425ac9c93100ab2c
Sarfaraj525 <sarfaraj525@gmail.com>
2025-01-13 17:32:53 +0600

    Blogs details  completely done

---
+++ src/components/view/BlogsDetails/BlogsDetailsHome.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import banner from "../../../../public/assets/mainBg.png";
+import style from "./BlogsDetails.module.css";
+
+const BlogsDetailsHome = () => {
+  return (
+    <div>
+      <div className={`${style.bgWrap} h-full`}>
+        <Image
+          alt="Banner"
+          src={banner}
+          placeholder="blur"
+          quality={100}
+          fill
+          sizes="100vw"
+          style={{
+            objectFit: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+      </div>
+      <div className={`${style.bgText} text-white max-w-[1400px] mx-auto h-full px-4`}>
+        <div>
+            <span className="text-6xl text-white font-bold">Blogs Details</span>
+            <p className="text-white text-lg font-normal mt-2">Home / Page / Blogs Details</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogsDetailsHome;
